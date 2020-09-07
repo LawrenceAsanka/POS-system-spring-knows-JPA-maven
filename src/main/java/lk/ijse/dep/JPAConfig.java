9768ea573ae492154bf1d06494033cd8b72f2722
LawrenceAsanka <asankalorance@gmail.com>
2020-09-07 19:48:05 +0530

    Enable Transaction

--- src/main/java/lk/ijse/dep/JPAConfig.java
+++ src/main/java/lk/ijse/dep/JPAConfig.java
@@ -13,10 +13,12 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+@EnableTransactionManagement
 @PropertySource("classpath:application.properties")
 @Configuration
 public class JPAConfig {
