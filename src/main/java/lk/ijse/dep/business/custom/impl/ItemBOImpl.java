12f174f788be002aa73cc3fbc8363708dd865351
LawrenceAsanka <asankalorance@gmail.com>
2020-09-07 19:16:20 +0530

    Rewamp ItemBOImpl

--- src/main/java/lk/ijse/dep/business/custom/impl/ItemBOImpl.java
+++ src/main/java/lk/ijse/dep/business/custom/impl/ItemBOImpl.java
@@ -2,43 +2,27 @@ package lk.ijse.dep.business.custom.impl;
 
 import lk.ijse.dep.business.custom.ItemBO;
 import lk.ijse.dep.dao.custom.ItemDAO;
-import lk.ijse.dep.db.JPAUtil;
 import lk.ijse.dep.entity.Item;
 import lk.ijse.dep.util.ItemTM;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Transactional
 public class ItemBOImpl implements ItemBO {
 
     // Dependency Declaration
     @Autowired
     private ItemDAO itemDAO;
 
+    @Transactional(readOnly = true)
     public String getNewItemCode() throws Exception {
-        EntityManagerFactory emf = JPAUtil.getEm();
-        EntityManager em = emf.createEntityManager();
-        String lastItemCode = null;
-        itemDAO.setEntityManger(em);
-        try {
-
-            em.getTransaction().begin();
-
-            lastItemCode = itemDAO.getLastItemCode();
-
-            em.getTransaction().commit();
-        } catch (Throwable t) {
-            em.getTransaction().rollback();
-            throw t;
-        } finally {
-            em.close();
-        }
+        String lastItemCode = itemDAO.getLastItemCode();
         if (lastItemCode == null) {
             return "I001";
         } else {
@@ -56,25 +40,9 @@ public class ItemBOImpl implements ItemBO {
         }
     }
 
+    @Transactional(readOnly = true)
     public List<ItemTM> getAllItems() throws Exception {
-
-        EntityManagerFactory emf = JPAUtil.getEm();
-        EntityManager em = emf.createEntityManager();
-        List<Item> allItems = null;
-        itemDAO.setEntityManger(em);
-        try {
-
-            em.getTransaction().begin();
-
-            allItems = itemDAO.findAll();
-
-            em.getTransaction().commit();
-        } catch (Throwable t) {
-            em.getTransaction().rollback();
-            throw t;
-        } finally {
-            em.close();
-        }
+        List<Item> allItems = itemDAO.findAll();
         List<ItemTM> items = new ArrayList<>();
         for (Item item : allItems) {
             items.add(new ItemTM(item.getCode(), item.getDescription(), item.getQtyOnHand(),
@@ -84,65 +52,15 @@ public class ItemBOImpl implements ItemBO {
     }
 
     public void saveItem(String code, String description, int qtyOnHand, double unitPrice) throws Exception {
-
-        EntityManagerFactory emf = JPAUtil.getEm();
-        EntityManager em = emf.createEntityManager();
-        itemDAO.setEntityManger(em);
-        try {
-
-            em.getTransaction().begin();
-
-            itemDAO.save(new Item(code, description, BigDecimal.valueOf(unitPrice), qtyOnHand));
-
-            em.getTransaction().commit();
-        } catch (Throwable t) {
-            em.getTransaction().rollback();
-            throw t;
-        } finally {
-            em.close();
-        }
-
+        itemDAO.save(new Item(code, description, BigDecimal.valueOf(unitPrice), qtyOnHand));
     }
 
     public void deleteItem(String itemCode) throws Exception {
-
-        EntityManagerFactory emf = JPAUtil.getEm();
-        EntityManager em = emf.createEntityManager();
-        itemDAO.setEntityManger(em);
-        try {
-
-            em.getTransaction().begin();
-
-            itemDAO.delete(itemCode);
-
-            em.getTransaction().commit();
-        } catch (Throwable t) {
-            em.getTransaction().rollback();
-            throw t;
-        } finally {
-            em.close();
-        }
-
+        itemDAO.delete(itemCode);
     }
 
     public void updateItem(String description, int qtyOnHand, double unitPrice, String itemCode) throws Exception {
-        EntityManagerFactory emf = JPAUtil.getEm();
-        EntityManager em = emf.createEntityManager();
-        itemDAO.setEntityManger(em);
-        try {
-
-            em.getTransaction().begin();
-
-            itemDAO.update(new Item(itemCode, description,
-                    BigDecimal.valueOf(unitPrice), qtyOnHand));
-
-            em.getTransaction().commit();
-        } catch (Throwable t) {
-            em.getTransaction().rollback();
-            throw t;
-        } finally {
-            em.close();
-        }
-
+        itemDAO.update(new Item(itemCode, description,
+                BigDecimal.valueOf(unitPrice), qtyOnHand));
     }
 }
