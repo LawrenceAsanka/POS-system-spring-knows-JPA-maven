23306e8f88ddf2e36f9eef989216a5cb3647b5d8
LawrenceAsanka <asankalorance@gmail.com>
2020-09-07 18:50:11 +0530

    Set datasource and JPAVendorAdapter

--- src/main/java/lk/ijse/dep/JPAConfig.java
+++ src/main/java/lk/ijse/dep/JPAConfig.java
@@ -1,7 +1,54 @@
 package lk.ijse.dep;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
+import javax.sql.DataSource;
+
+@PropertySource("classpath:application.properties")
 @Configuration
 public class JPAConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds) {
+        LocalContainerEntityManagerFactoryBean lcefmb = new LocalContainerEntityManagerFactoryBean();
+        lcefmb.setDataSource(ds);
+        // set the persistence.xml values
+        lcefmb.setJpaVendorAdapter(jpaVendorAdapter());
+        return lcefmb;
+    }
+
+    //set some application.property values through this method
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName(env.getRequiredProperty("javax.persistence.jdbc.driver"));
+        ds.setUrl(env.getRequiredProperty("javax.persistence.jdbc.url"));
+        ds.setUsername(env.getRequiredProperty("javax.persistence.jdbc.user"));
+        ds.setPassword(env.getRequiredProperty("javax.persistence.jdbc.password"));
+        return ds;
+    }
+
+    // set META-INF and some application.property values through this method
+    private JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setDatabasePlatform(env.getRequiredProperty("hibernate.dialect"));
+        adapter.setShowSql(env.getRequiredProperty("hibernate.show_sql", Boolean.class));
+        adapter.setGenerateDdl(env.getRequiredProperty("hibernate.hbm2ddl.auto").equals("update") ? true : false);
+        return adapter;
+    }
+
+
 }
