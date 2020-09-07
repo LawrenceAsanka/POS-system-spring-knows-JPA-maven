235506ce8f2629081f27ad5127a17813e0364135
LawrenceAsanka <asankalorance@gmail.com>
2020-09-07 19:13:46 +0530

    Rewamp customerBOImpl

--- src/main/java/lk/ijse/dep/business/custom/impl/CustomerBOImpl.java
+++ src/main/java/lk/ijse/dep/business/custom/impl/CustomerBOImpl.java
@@ -2,18 +2,17 @@ package lk.ijse.dep.business.custom.impl;
 
 import lk.ijse.dep.business.custom.CustomerBO;
 import lk.ijse.dep.dao.custom.CustomerDAO;
-import lk.ijse.dep.db.JPAUtil;
 import lk.ijse.dep.entity.Customer;
 import lk.ijse.dep.util.CustomerTM;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Transactional
 public class CustomerBOImpl implements CustomerBO {
 
 
@@ -21,25 +20,9 @@ public class CustomerBOImpl implements CustomerBO {
     @Autowired
     private CustomerDAO customerDAO;
 
+    @Transactional(readOnly = true)
     public List<CustomerTM> getAllCustomers() throws Exception {
-        EntityManagerFactory emf = JPAUtil.getEm();
-        EntityManager em = emf.createEntityManager();
-        customerDAO.setEntityManger(em);
-        List<Customer> allCustomers = null;
-        try {
-
-            em.getTransaction().begin();
-
-            allCustomers = customerDAO.findAll();
-
-            em.getTransaction().commit();
-        } catch (Throwable t) {
-            em.getTransaction().rollback();
-            throw t;
-        } finally {
-            em.close();
-        }
-
+        List<Customer> allCustomers = customerDAO.findAll();
         List<CustomerTM> customers = new ArrayList<>();
         for (Customer customer : allCustomers) {
             customers.add(new CustomerTM(customer.getId(), customer.getName(), customer.getAddress()));
@@ -49,84 +32,20 @@ public class CustomerBOImpl implements CustomerBO {
     }
 
     public void saveCustomer(String id, String name, String address) throws Exception {
-        EntityManagerFactory emf = JPAUtil.getEm();
-        EntityManager em = emf.createEntityManager();
-        customerDAO.setEntityManger(em);
-        try {
-
-            em.getTransaction().begin();
-
-            customerDAO.save(new Customer(id, name, address));
-            em.getTransaction().commit();
-        } catch (Throwable t) {
-            em.getTransaction().rollback();
-            throw t;
-        } finally {
-            em.close();
-        }
-
-
+        customerDAO.save(new Customer(id, name, address));
     }
 
     public void deleteCustomer(String customerId) throws Exception {
-        EntityManagerFactory emf = JPAUtil.getEm();
-        EntityManager em = emf.createEntityManager();
-        customerDAO.setEntityManger(em);
-        try {
-
-            em.getTransaction().begin();
-
-            customerDAO.delete(customerId);
-            em.getTransaction().commit();
-        } catch (Throwable t) {
-            em.getTransaction().rollback();
-            throw t;
-        } finally {
-            em.close();
-        }
-
+        customerDAO.delete(customerId);
     }
 
     public void updateCustomer(String name, String address, String customerId) throws Exception {
-        EntityManagerFactory emf = JPAUtil.getEm();
-        EntityManager em = emf.createEntityManager();
-        customerDAO.setEntityManger(em);
-        try {
-
-            em.getTransaction().begin();
-
-            customerDAO.update(new Customer(customerId, name, address));
-            em.getTransaction().commit();
-        } catch (Throwable t) {
-            em.getTransaction().rollback();
-            throw t;
-        } finally {
-            em.close();
-        }
-
-
+        customerDAO.update(new Customer(customerId, name, address));
     }
 
+    @Transactional(readOnly = true)
     public String getNewCustomerId() throws Exception {
-        EntityManagerFactory emf = JPAUtil.getEm();
-        EntityManager em = emf.createEntityManager();
-        customerDAO.setEntityManger(em);
-        String lastCustomerId = null;
-
-        try {
-
-            em.getTransaction().begin();
-
-            lastCustomerId = customerDAO.getLastCustomerId();
-
-            em.getTransaction().commit();
-        } catch (Throwable t) {
-            em.getTransaction().rollback();
-            throw t;
-        } finally {
-            em.close();
-        }
-
+        String lastCustomerId = customerDAO.getLastCustomerId();
         if (lastCustomerId == null) {
             return "C001";
         } else {
